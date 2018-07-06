3342570f004ab2a2afc93e3c4e83241e2a04ceeb
faithzzz <1208674494@qq.com>
2018-07-06 20:00:00 +0800

    ..

--- .babelrc.js
+++ .babelrc.js
@@ -1,11 +1,15 @@
-{
-  "plugins": [
-    ["import",{"libraryName": "antd", "style": true}]
-  ],
-  "presets": ["react", "es2015"],
-  "env": {
-      "development" : {
-        "compact": false
-      }
-    }
-}
+module.exports = {
+  plugins: [
+    [
+      'transform-es2015-modules-commonjs',
+      'babel-plugin-transform-react-jsx',
+      'babel-plugin-module-resolver',
+      {
+        alias: {
+          components: './src/components',
+        }
+      },
+    ],
+    //["import",{libraryName: "antd", style: true}] // antd按需加载
+  ]
+};
