3b4174f0e8b329931e734734c9a2f88970b065ce
yimu <1208674494@qq.com>
2018-08-25 18:51:49 +0800

    .

--- src/mobx/index.js
+++ src/mobx/index.js
@@ -1,16 +1,3 @@
-import { observable,action } from 'mobx';
 
-var appState = observable({
-	timer: 0
-})
 
-appState.resetTimer = () => {
-	appState.timer = 0;
-}
-
-setInterval(action(function tick() {
-	debugger
-    appState.timer += 1;
-}), 1000);
-
-export default appState;
+export default ./todo.js;
